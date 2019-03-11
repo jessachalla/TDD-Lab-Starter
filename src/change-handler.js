e29e3d6d8ab429e83c313f5f54fb0443fa458082
jessachalla <jessa.challa@gvmc.org>
2019-03-11 15:15:46 -0400

    first commit

---
+++ src/change-handler.js
@@ -0,0 +1,77 @@
+/**
+ * This class handles change for a vending machine.
+ * 
+ * IMPORTANT: All amounts are in cents. E.g. $1.35 = 135. This will help with rounding errors.
+ */
+class ChangeHandler {
+
+    constructor(amountDue, cashTendered) {
+        this.amountDue = amountDue;
+        this.cashTendered = cashTendered;
+    }
+
+    /**
+     * The customer inserts a coin, increasing the cashTendered.
+     * The parameter "type" is a string that is either quarter, dime, nickel, or penny
+     */
+    insertCoin(type) {
+        if (type === "penny") {
+            return this.cashTendered + 1;
+        }
+        if (type === "nickel") {
+            return this.cashTendered + 5;
+        }
+        if (type === "dime") {
+            return this.cashTendered + 10;
+        }
+        if (type === "quarter") {
+            return this.cashTendered + 25;
+        }
+    }
+
+    /**
+     * Returns true if enough coins have been inserted to at least meet the amountDue
+     */
+    isPaymentSufficient(cash) {
+        if (cash >= this.amountDue) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    giveChange() {
+        // TODO return the correct change in the following format...
+        let numQuarters = 0;
+        let numDimes = 0;
+        let numNickels = 0;
+        let numPennies = 0;
+        let change = this.cashTendered - this.amountDue;
+
+        while (change > 0) {
+            if (change >= 25) {
+                change -= 25;
+                numQuarters += 1;
+            }
+            else if (change >= 10) {
+                change -= 10;
+                numDimes += 1;
+            }
+            else if (change >= 5) {
+                change -= 5;
+                numNickels += 1;
+            }
+            else if (change >= 1) {
+                change -= 1;
+                numPennies += 1;
+            }
+        }
+
+        return {
+            quarters: numQuarters,
+            dimes: numDimes,
+            nickels: numNickels,
+            pennies: numPennies
+        }
+    }
+}
---
+++ tests/change-handler.spec.js
@@ -0,0 +1,48 @@
+// These test cases are all unfinished. We will finish them together.
+describe("tests for change-handler", function() {
+
+    // Set up a test below...
+    it("does something (insertCoin)- inserts a coin and increases each cash tendered", function() {
+        // Remember, you can arrange, act, and assert...start small
+        //Arrange
+        let changeHandler = new ChangeHandler(0,0);
+        //Act and Assert
+        expect(changeHandler.insertCoin('penny')).toBe(1);
+        expect(changeHandler.insertCoin('nickel')).toBe(5);
+        expect(changeHandler.insertCoin('dime')).toBe(10);
+        expect(changeHandler.insertCoin('quarter')).toBe(25);
+    });
+
+    it("Returns true if enough coins have been inserted to at least meet the amountDue", function() {
+        // Remember, you can arrange, act, and assert...start small
+        //Arrange
+        let changeHandler = new ChangeHandler(20,0);
+
+        expect(changeHandler.isPaymentSufficient('3')).toBe(false);
+        expect(changeHandler.isPaymentSufficient('20')).toBe(true);
+        expect(changeHandler.isPaymentSufficient('100')).toBe(true);
+    });
+
+    it("return the correct change", function() {
+        //The amount due was 100, and you gave the machine 125, you get back 25
+        let changeHandler = new ChangeHandler(100,125);
+        expect(changeHandler.giveChange()).toEqual({quarters: 1, dimes: 0, nickels: 0, pennies: 0 });
+
+        //The amout due was 33, you gave the machine 100, you get back 67
+        changeHandler = new ChangeHandler(33,100);
+        expect(changeHandler.giveChange()).toEqual({quarters: 2, dimes: 1, nickels: 1, pennies: 2 });
+
+        //The amout due was 9, you gave the machine 10, you get back 1
+        changeHandler = new ChangeHandler(9,10);
+        expect(changeHandler.giveChange()).toEqual({quarters: 0, dimes: 0, nickels: 0, pennies: 1 });
+
+        //The amout due was 15, you gave the machine 15, you get back 0
+        changeHandler = new ChangeHandler(15,15);
+        expect(changeHandler.giveChange()).toEqual({quarters: 0, dimes: 0, nickels: 0, pennies: 0 });
+
+        //The amout due was 80, you gave the machine 70, you get back 0 because you havent paid enough
+        changeHandler = new ChangeHandler(80,70);
+        expect(changeHandler.giveChange()).toEqual({quarters: 0, dimes: 0, nickels: 0, pennies: 0 });
+    });
+
+});
